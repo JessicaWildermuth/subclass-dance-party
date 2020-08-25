35a7319012b34ab049d39bf051f8f10a0b7c2863
Carina Zhang <carinalxx@gmail.com>
2020-08-25 16:03:50 -0700

    ocean dance party completed

--- src/otherDancer1.js
+++ src/otherDancer1.js
@@ -3,28 +3,15 @@ var SlidingDancer = function(top, left, timeBetweenSteps) {
 
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('sliding');
-
-
 };
 
 SlidingDancer.prototype = Object.create(Dancer.prototype);
 SlidingDancer.prototype.constructor = SlidingDancer;
 
-// SlidingDancer.prototype.lineUp = function(top, left) {
-
-//   var styleSettings = {
-//     left: left,
-//     top: top
-//   };
-//   this.$node.css(styleSettings);
-//   this.$node.setPosition(top, left);
-//   // $('.inLine').append(this.$node);
-// };
 
 SlidingDancer.prototype.step = function() {
 
   Dancer.prototype.step.call(this);
-  this.$node.toggle();
-  // this.$node.effect('shake', { direction: 'right', times: 1, distance: 300}, 1000 );
-
+  // this.$node.toggle();
+  this.$node.animate({ 'left': '-750px', 'right': '-=700px', 'left': '-1500px' }, 8000);
 };
